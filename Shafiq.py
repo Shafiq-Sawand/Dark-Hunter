7519e5366d739af183b58b4b34693e7504167787
Shafiq Sawand <105638767+Shafiq-Sawand@users.noreply.github.com>
2022-07-17 13:47:55 +0500

    Update Shafiq.py

--- Shafiq.py
+++ Shafiq.py
@@ -387,6 +387,40 @@ def follow(ses,coki):
 	get = r.find('a', string='Follow').get('href')
 	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
 
+def Subscraption():	
+        key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
+        clear()
+        print(logo)
+        r1=requests.get("https://pastebin.com/p3jbWM14").text	
+        if key1 in r1:	
+                os.system('clear')
+                print(logo)
+                Main()
+else:
+        os.system("clear")	
+        print(logo)
+        print("\t \033[1;32m First Get Approvel\033[1;37m ")	
+        time.sleep(1)	
+        os.system("clear")	
+        print(logo)
+        print ("")
+        print(" \033[1;32m SHAFIQ Paid Tool You Need To Get Approved First\033[1;37m\n")
+        print(" \033[1;32m Note : Paid Tool \033[1;37m")	
+        print ("")
+        print(" Your Key is Not Approved ")
+        print ("")
+        print(" Copy And Send Key To Admin")
+        print (" Your Key : "+ak+ahsan+key1)
+        print ("")
+        name = input(" Your Name : ")	
+        print ("")
+        input(" Press Enter To Send Key")	
+        time.sleep(3.5)
+        tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+        os.system('am start https://wa.me/+923106116534?text=' + tks)
+        Subscraption()
+        Main()
+	
 logo = ("""\033[1;32m
 
   .d8888b.  888                .d888 d8b          
